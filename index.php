9546caa9e5a4ba5f3e500a8c15a87a43dcb70556
Your Name <you@example.com>
2025-01-14 15:56:58 +0300

    Убрал из title свои реквизиты

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Расчет Периметра Треугольника Русяев Я.С.</title>
+  <title>Расчет Периметра Треугольника</title>
     <style>
         body {
             font-family: sans-serif;
